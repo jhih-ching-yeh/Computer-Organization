11eb9dde5f5e47a04efd1c456b20cf3b97e9c206
jhih-ching-yeh <61455441+jhih-ching-yeh@users.noreply.github.com>
2022-02-11 14:54:17 +0800

    Add files via upload

---
+++ lab3/Instr_Memory.v
@@ -0,0 +1,39 @@
+// 0613144¸­¤§´¸  0613149 ¯Î¦Ð«G
+
+//Subject:     CO project 3 - Simple Single CPU
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      0613144¸­¤§´¸  0613149 ¯Î¦Ð«G
+//----------------------------------------------
+//Date:        108.05.15
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+`timescale 1ns / 1ps
+module Instr_Memory(
+    addr_i,
+	instr_o
+);
+ 
+// Interface
+input  [31:0]	addr_i;
+output [31:0]	instr_o;
+
+// Internal Signals
+integer          i;
+
+// Instruction File
+reg		[31:0]		instruction_file	[0:31];
+
+//Initial Memory Contents
+initial begin
+    for ( i=0; i<32; i=i+1 )
+	    instruction_file[i] = 32'b0;
+    $readmemb("CO_P3_test_data1.txt", instruction_file);  //Read instruction from "CO_P3_test_data.txt"   
+		
+end
+
+assign	instr_o = instruction_file[addr_i/4];
+
+endmodule
